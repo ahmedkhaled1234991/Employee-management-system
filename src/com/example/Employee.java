cca177878f5948769fa3a7ddf3bec14bbb1c29b1
Ahmed khaled <ahmedkhaled1234991@gmail.com>
2024-07-05 18:06:45 +0300

    first commit

---
+++ src/com/example/Employee.java
@@ -0,0 +1,57 @@
+package com.example;
+
+public class Employee {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private int age;
+    private double salary;
+
+    public Employee(String name, String email, String phoneNumber, int age, double salary) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
